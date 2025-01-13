206b52dda310f2b80fa5cfa63d181f5b998fd0df
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2025-01-13 17:11:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { TypeAnimation } from 'react-type-animation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedin, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
-import './Home.css';
+
 
 import logo from './assets/wmremove-transformed.jpeg';
 import img1 from './assets/htmlnew.webp';
@@ -49,21 +49,21 @@ export default function App() {
     return (
         <>
             <nav className="navbar">
-                <img src={logo} alt="Logo" className="logo" />
-                <button className="mobile-menu-toggle" onClick={toggleMenu}>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </button>
-                <div className={`nav-links ${isMobileMenuOpen ? 'show' : ''}`}>
-                    <a href="#home" className="nav-link">Home</a>
-                    <a href="#about" className="nav-link">About Me</a>
-                    <a href="#skills" className="nav-link">Skills</a>
-                    <a href="#projects" className="nav-link">Projects</a>
-                    <a href="#experience" className="nav-link">Experience</a>
-                    <a href="#contact" className="nav-link">Contact Me</a>
-                </div>
-            </nav>
+    <img src={logo} alt="Logo" className="logo" />
+    <button className="mobile-menu-toggle" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+    </button>
+    <div className={`nav-links ${isMobileMenuOpen ? 'show' : ''}`}>
+        <a href="#home" className="nav-link">Home</a>
+        <a href="#about" className="nav-link">About Me</a>
+        <a href="#skills" className="nav-link">Skills</a>
+        <a href="#projects" className="nav-link">Projects</a>
+        <a href="#experience" className="nav-link">Experience</a>
+        <a href="#contact" className="nav-link">Contact Me</a>
+    </div>
+</nav>
 
             <div className="main-content">
                 <section id="home" className="section">
